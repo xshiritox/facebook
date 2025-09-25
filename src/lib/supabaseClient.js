0dc1945de24ed9efee5e18abd2e10aaadbdeb40d
Marco Antonio Perez Infante <mapi27022005@gmail.com>
2025-09-25 18:27:53 -0400

    *

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Configuración directa sin variables de entorno
-const supabaseUrl = 'https://tcnmbvpfensnfjjebwzk.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbm1idnBmZW5zbmZqamVid3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NjkyNjcsImV4cCI6MjA3NDM0NTI2N30.9OsQxrdkqUIUifZivVgFl5HrnlVsCVtJmidMdm9_lHU'
+const supabaseUrl = 'https://nbaujlouhlihofuotcvf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5iYXVqbG91aGxpaG9mdW90Y3ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MzU1NDcsImV4cCI6MjA3NDQxMTU0N30.Io6nlpI8gY0rOgV8-BO3TZA7BodocOJrjK2YlHFFGB8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
